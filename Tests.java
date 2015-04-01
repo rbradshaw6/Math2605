a56b47e4db795f30b9da5874bd7431dbf65f3ae7
John Blum <jblum@gatech.edu>
2015-03-31 23:47:36 -0400

    tits
    nips

--- Tests.java
+++ Tests.java
@@ -4,5 +4,18 @@ public class Tests {
 
     public static void main(String[] args) {
         // Tests go here.
+        
+        //2x2 test for QR
+        double[][] m1 = {{1, 1}, {1, 2}};
+        Matrix m = new Matrix(m1);
+        m.qr_fact_givens();
+        m.qr_fact_househ();
+        
+        //2x2 test for LU
+        double[][] m3 = {{1, 1}, {1, 2}};
+        Matrix m2 = new Matrix(m1);
+        m2.lu_fact();
+        
+        
     }
 }
